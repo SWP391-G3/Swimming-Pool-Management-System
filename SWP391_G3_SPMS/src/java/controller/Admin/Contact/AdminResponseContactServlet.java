b90ebd71e3e3f7efe1c11a5e4681abba3792de66
CSHuy1611 <he186445caosyhuy@gmail.com>
2025-07-13 12:26:22 +0700

    demo complete servlet get contact by id and forward in jsp

--- SWP391_G3_SPMS/src/java/controller/Admin/Contact/AdminResponseContactServlet.java
+++ SWP391_G3_SPMS/src/java/controller/Admin/Contact/AdminResponseContactServlet.java
@@ -2,9 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package controller.Admin.Contact;
 
+import dao.admin.ContactWithAdminDAO;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
@@ -12,41 +12,45 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import model.admin.ContactWithAdmin;
 
 /**
  *
  * @author Lenovo
  */
-@WebServlet(name="AdminResponseContactServlet", urlPatterns={"/adminResponseContact"})
+@WebServlet(name = "AdminResponseContactServlet", urlPatterns = {"/adminResponseContact"})
 public class AdminResponseContactServlet extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet AdminResponseContactServlet</title>");  
+            out.println("<title>Servlet AdminResponseContactServlet</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet AdminResponseContactServlet at " + request.getContextPath () + "</h1>");
+            out.println("<h1>Servlet AdminResponseContactServlet at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
-    } 
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -54,12 +58,23 @@ public class AdminResponseContactServlet extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        processRequest(request, response);
-    } 
+            throws ServletException, IOException {
+        String contact_idRaw = request.getParameter("id");
+        ContactWithAdminDAO dao = new ContactWithAdminDAO();
+        int contact_id;
+        try {
+            contact_id = Integer.parseInt(contact_idRaw);
+        } catch (NumberFormatException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+        ContactWithAdmin cw = dao.getContactWithAdmin(contact_id);
+        request.setAttribute("cw", cw);
+        request.getRequestDispatcher("adminResponseContact.jsp").forward(request, response);
+    }
 
-    /** 
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -67,12 +82,13 @@ public class AdminResponseContactServlet extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
